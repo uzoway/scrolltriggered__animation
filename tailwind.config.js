877e67a1faa612488be91b41a8516cad96f86a4b
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2023-07-07 15:56:12 +0100

    docs: Set up project folder

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
